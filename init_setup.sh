932a6139cc5d92372d7955fc49fa84d04da05447
mohammadusama2 <mohammadusama2@hotmail.com>
2022-10-22 21:56:23 +0530

    initial setup done

--- init_setup.sh
+++ init_setup.sh
@@ -1,17 +1,25 @@
-echo [$(date)]: "START"
-echo [$(date)]: "creating environment"
-conda create --prefix ./env python=3.7 -y
-echo [$(date)]: "activate environment"
-source activate ./env
-echo [$(date)]: "install requirements"
-pip install -r requirements.txt
-echo [$(date)]: "export conda environment"
-conda env export > conda.yaml
-echo "# ${PWD}" > README.md
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
-echo [$(date)]: "END"
+# echo [$(date)]: "START"
+# echo [$(date)]: "creating environment"
+# conda create --prefix ./env python=3.8 -y
+# echo [$(date)]: "activate environment"
+# source activate ./env
+echo [$(date)]: "create folder and file structure"
 
-# to remove everything -
-# rm -rf env/ .gitignore conda.yaml README.md .git/
+for DIR in data_ingestion data_preparation data_validation model model_evaluation model_trainer
+do 
+    echo [$(date)]: "Creating", src/"NER_"$DIR
+    mkdir -p src/"NER_"$DIR
+    echo [$(date)]: "Creating __init__.py inside above folders"
+    touch src/"NER_"$DIR/"__init__.py" src/"NER_"$DIR/$DIR".py"
+    echo "from src.NER_"$DIR"."$DIR" import *" > src/"NER_"$DIR/"__init__.py"
+done    
+
+# echo [$(date)]: "install requirements"
+# pip install -r requirements.txt -q
+# echo [$(date)]: "install pytorch dependency"
+# pip install torch --extra-index-url https://download.pytorch.org/whl/cu113 -q
+
+# echo [$(date)]: "END"
+
+# # to remove everything -
+# # rm -rf env/ .gitignore conda.yaml README.md .git/
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "FSDS_NLP_NER_Project"
+AUTHOR_USER_NAME = "mohammadusama2"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -14,10 +14,10 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="A small package for NER Transformers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/mohammadusama2/FSDS_NLP_NER_Project",
     author_email="sunny.c17hawke@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
